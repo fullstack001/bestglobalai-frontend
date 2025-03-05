e2cae81aedee22fce4ef7a3fac1f1b231fbc9010
fullstack001 <mr.dimas332@gmail.com>
2025-03-05 10:06:14 -0600

    fix complex

--- src/Page/Landing/Extra.js
+++ src/Page/Landing/Extra.js
@@ -2,85 +2,10 @@ import React from "react";
 import { IoIosArrowForward, IoIosCheckmarkCircleOutline } from "react-icons/io";
 import { useDispatch } from "react-redux";
 import extra_background from "../../assets/images/landing/extra_background.png";
-import { IoIosArrowForward, IoIosCheckmarkCircleOutline } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
-const extraData = [
-  {
-    title: "API Installation",
-    description:
-      "The assigned developer will negotiate with you to determine what you need to get the best integration on your website and to match with your business model. The price may go up or down based on what you have ordered.",
-    price: "$250.00 each",
-    features: [
-      "Over 128 API choices in 12 business categories.",
-      "Qualified Fullstack Developer matched to your business model.",
-      "Goal-focused recommendations.",
-      "AI-powered automation and guaranteed results.",
-      "Negotiations secure and invoicing detailed.",
-    ],
-  },
-  {
-    title: "Professional Design of Your ePub3 Landing Page Promotion",
-    description:
-      "The assigned designer will negotiate with you to determine what you need for your landing page based on the creative elements you want to add for a specific social marketing campaign. They will match the creative concepts based on your business model and product/service.",
-    price: "$250.00 each",
-    features: [
-      "Created inside your account with the Embellisher editor.",
-      "Qualified Professional Graphics and Video Designer.",
-      "Matched to your campaign audience and objectives.",
-      "SEO/SEM created for your landing page.",
-      "Advanced analytics and insights.",
-    ],
-  },
-  {
-    title:
-      "Professional Design of Your Business Website Using Modern Styles and Technologies",
-    description:
-      "The assigned designer/developer will negotiate with you to determine what you need for your new business model and/or the renovation of your old business model and product/service. The price may go up or down based on your requirements.",
-    price: "$250.00 each",
-    features: [
-      "Created on your website location with latest AI technologies.",
-      "Qualified Professional Business Website Designer.",
-      "Matched to your business objectives and brand model.",
-      "SEO/SEM created for your home page.",
-      "Guaranteed to match your design needs and instructions.",
-    ],
-  },
-  {
-    title:
-      "Professional Search Engine Optimization (SEO) for your business or promotion campaign.",
-    description:
-      "The assigned SEO professional will assess and negotiate with you to determine what you need for your campaign and promotion. The SEO can apply to your business and for your campaign.",
-    price: "$250.00 each",
-    features: [
-      "Created for your chosen marketing campaign or webpage.",
-      "Qualified Professional SEO/SEM developer assigned.",
-      "Matched to your campaign audience and objectives.",
-      "Meets your business model campaign goals and objectives.",
-      "Advanced analytics and insights.",
-    ],
-  },
-  {
-    title: "Order the Conversational Chat Avatar with 'knowledge base.'",
-    description:
-      "The $100 fee is for use of the default avatars available in the paid plan. The $200 fee is for the creation of your selfie video sample that we'll use to create your Chat Avatar.",
-    price: "$100.00 each",
-    features: [
-      "Depending on your business brand, we'll develop the knowledge base content.",
-      "You select which default template you want to use in your chat or for the $200, create your own 2-3 minute selfie video.",
-      "Length of your chat avatar streams is based on the following calculation.",
-      "When streaming an interactive avatar, 1 API credit equals 5 minutes of streaming.",
-    ],
-  },
-  {
-    title: "Order the PureVPN server for one month.",
-    description:
-      "The $10 fee is for use of the VPN server when you want to bypass hackers and spies online. Market to your social media networds without fear. Just use the coupon to redeem your month's worth of protection and use it whenever you have that special social media post to send over a private network. We also have other time limits, so just email us to see what they are. ",
-    price: "$10.00 each",
-    features: [
-      "A coupon is redeemable for a month's PureVPN server use. You'll get the link to go there and get your website version.",
-    ],
-  },
-];
+import { setExtra } from "../../store/getExtra";
+import { extraData } from "../../lib/extraData";
 
 const Extra = () => {
   const dispatch = useDispatch();
